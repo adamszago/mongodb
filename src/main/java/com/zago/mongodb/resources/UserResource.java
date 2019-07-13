54ab216ab03e1f35fb9ffd1411e48e951bb87faf
Adams Zago <adams.zago@gmail.com>
2019-07-13 16:50:37 -0300

    Creating exceptions and exceptions handler

--- src/main/java/com/zago/mongodb/resources/UserResource.java
+++ src/main/java/com/zago/mongodb/resources/UserResource.java
@@ -1,10 +1,11 @@
 package com.zago.mongodb.resources;
 
-import java.util.List;import java.util.stream.Collector;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,18 +15,24 @@ import com.zago.mongodb.dto.UserDTO;
 import com.zago.mongodb.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
-	//Poderia ser também @GetMapping
-	@RequestMapping(method=RequestMethod.GET)
+
+	// Poderia ser também @GetMapping
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = service.findAdll();
 		List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return  ResponseEntity.ok().body(usersDTO);
-		
+		return ResponseEntity.ok().body(usersDTO);
+	}
+
+	// Poderia ser também @GetMapping
+	@RequestMapping(value="/{id}",  method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
