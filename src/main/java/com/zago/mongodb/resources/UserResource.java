4f30e0694708a8ae196c3c5e9fd746f0d6d34b1b
Adams Zago <adams.zago@gmail.com>
2019-07-13 12:45:11 -0300

    Creating user and userResource

---
+++ src/main/java/com/zago/mongodb/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.zago.mongodb.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zago.mongodb.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	//Poderia ser também @GetMapping
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Green", "alex@gmail.com");
+		
+		List<User> users = new ArrayList<>();
+		users.addAll(Arrays.asList(maria, alex));
+		return  ResponseEntity.ok().body(users);
+		
+	}
+}
